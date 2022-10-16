atweiden/voidpkgs

commit f8c657fe21045f49128b133d62f7afec95f3a323

xbps-src: add update-local and show-local-updates
https://github.com/void-linux/void-packages/commit/4c43245e0b989fa85fe57090a1a584d6e6650546

common/xbps-src/shutils/bulk.sh
@@ -54,7 +54,7 @@ bulk_sortdeps() {
 }
 
 bulk_build() {
-    local sys="$1"
+    local cmd="$1"
     local NPROCS=$(($(nproc)*2))
     local NRUNNING=0
 
@@ -67,10 +67,17 @@ bulk_build() {
     fi
 
     # Compare installed pkg versions vs srcpkgs
-    if [[ $sys ]]; then
-        bulk_sortdeps $(xbps-checkvers -f '%n' -I -D $XBPS_DISTDIR)
+    case "$cmd" in
+    installed)
+        bulk_sortdeps $(xbps-checkvers -f '%n' -I -D "$XBPS_DISTDIR")
         return $?
-    fi
+        ;;
+    local)
+        bulk_sortdeps $(xbps-checkvers -f '%n' -i -R "${XBPS_REPOSITORY}" -R "${XBPS_REPOSITORY}/nonfree" -D "$XBPS_DISTDIR")
+        return $?
+        ;;
+    esac
+
     # compare repo pkg versions vs srcpkgs
     for f in $(xbps-checkvers -f '%n' -D $XBPS_DISTDIR); do
         if [ $NRUNNING -eq $NPROCS ]; then
@@ -90,9 +97,9 @@ bulk_build() {
 }
 
 bulk_update() {
-    local args="$1" pkgs f rval
+    local cmd="$1" pkgs f rval
 
-    pkgs="$(bulk_build ${args})"
+    pkgs="$(bulk_build "${cmd}")"
     [[ -z $pkgs ]] && return 0
 
     msg_normal "xbps-src: the following packages must be rebuilt and updated:\n"
@@ -112,7 +119,7 @@ bulk_update() {
             msg_error "xbps-src: failed to build $pkgver pkg!\n"
         fi
     done
-    if [ -n "$pkgs" -a -n "$args" ]; then
+    if [ -n "$pkgs" -a "$cmd" == installed ]; then
         echo
         msg_normal "xbps-src: updating your system, confirm to proceed...\n"
         ${XBPS_SUCMD} "xbps-install --repository=$XBPS_REPOSITORY --repository=$XBPS_REPOSITORY/nonfree -u ${pkgs//[$'\n']/ }" || return 1

xbps-src
@@ -122,6 +122,9 @@ show-repo-updates
 show-sys-updates
     Prints the list of outdated packages in your system.
 
+show-local-updates
+    Prints the list of outdated packages in your local repositories.
+
 sort-dependencies <pkg> <pkgN+1> ...
     Given a list of packages specified as additional arguments, a sorted dependency
     list will be returned to stdout.
@@ -131,6 +134,9 @@ update-bulk
 
 update-sys
     Rebuilds all packages in your system that are outdated and updates them.
+
+update-local
+    Rebuilds all packages in your local repositories that are outdated.
 
 update-check <pkgname>
     Check upstream site of <pkgname> for new releases.
@@ -961,7 +967,10 @@ case "$XBPS_TARGET" in
         bulk_build
         ;;
     show-sys-updates)
-        bulk_build -I
+        bulk_build installed
+        ;;
+    show-local-updates)
+        bulk_build local
         ;;
     sort-dependencies)
         bulk_sortdeps ${@/$XBPS_TARGET/}
@@ -970,7 +979,10 @@ case "$XBPS_TARGET" in
         bulk_update
         ;;
     update-sys)
-        bulk_update -I
+        bulk_update installed
+        ;;
+    update-local)
+        bulk_update local
         ;;
     update-check)
         read_pkg ignore-problems

commit 6bf38bb17fc76ddee53f990c9f9d4fc1b1f5a7b4

xbps-src: use unique names for bulk update commands
A regression introduced in f8c657fe21 is causing the 'xbps-src update-sys'
command to skip the final step of installing the updated packages.

This happens because the `cmd` variable name clashes with a loop
variable used in common/environment/setup/install.sh script (line 16).
Thus the `"$cmd" == installed` comparison
(common/xbps-src/shutils/bulk.sh line 122) fails and the installation
step is skipped.

Using unique variable names in `bulk.sh` avoids this problem.

https://github.com/void-linux/void-packages/commit/7c3f5a15b96fc880792e84a51f836f0e44741ea5

common/xbps-src/shutils/bulk.sh
@@ -54,7 +54,7 @@ bulk_sortdeps() {
 }
 
 bulk_build() {
-    local cmd="$1"
+    local bulk_build_cmd="$1"
     local NPROCS=$(($(nproc)*2))
     local NRUNNING=0
 
@@ -67,7 +67,7 @@ bulk_build() {
     fi
 
     # Compare installed pkg versions vs srcpkgs
-    case "$cmd" in
+    case "$bulk_build_cmd" in
     installed)
         bulk_sortdeps $(xbps-checkvers -f '%n' -I -D "$XBPS_DISTDIR")
         return $?
@@ -97,9 +97,9 @@ bulk_build() {
 }
 
 bulk_update() {
-    local cmd="$1" pkgs f rval
+    local bulk_update_cmd="$1" pkgs f rval
 
-    pkgs="$(bulk_build "${cmd}")"
+    pkgs="$(bulk_build "${bulk_update_cmd}")"
     [[ -z $pkgs ]] && return 0
 
     msg_normal "xbps-src: the following packages must be rebuilt and updated:\n"
@@ -119,7 +119,7 @@ bulk_update() {
             msg_error "xbps-src: failed to build $pkgver pkg!\n"
         fi
     done
-    if [ -n "$pkgs" -a "$cmd" == installed ]; then
+    if [ -n "$pkgs" -a "$bulk_update_cmd" == installed ]; then
         echo
         msg_normal "xbps-src: updating your system, confirm to proceed...\n"
         ${XBPS_SUCMD} "xbps-install --repository=$XBPS_REPOSITORY --repository=$XBPS_REPOSITORY/nonfree -u ${pkgs//[$'\n']/ }" || return 1